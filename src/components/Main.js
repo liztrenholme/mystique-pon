c14b3693c8a13d5434a4489ba3e89868bc52fbad
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-08-27 22:03:59 -0400

    font changes, style tweaks on main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ class Main extends Component {
     return (
       <div className="Main">
         <div className="welcome">
-          <img src={Playing} alt="playing puppy" width="50%" />
+          <img src={Playing} alt="playing puppy" height="500px" className="playing-image" />
         </div>
       </div>
     );
