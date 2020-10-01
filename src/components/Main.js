23626609a4beb0db259fa284d210170a77999e87
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-09-30 23:33:11 -0400

    added logo above parallax, reworked main formatting and styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,19 +16,22 @@ class Main extends Component {
     const vidUrl = 'https://www.youtube.com/watch?v=KiHCIIXhr0c';
     return (
       <div className="Main">
-        <div className='parallax-bg' />
-        <div className="welcome">
+        <div className='main-title-box'>
+          <h1 className='main-title'>Enchanting PONs</h1>
+          <h2>Polish Lowland Sheepdogs</h2>
+        </div>
+        <div className='parallax-bg'>
+        </div>
 
-          
-          <div className="row lineup-row">
-            <div className="col-md-7">
-              <div className="puppy-lineup fade">
-                <img className="together" src={MomAndPups} alt="mom and puppies together" />
-              </div>
-            </div>
-            <div className="col-md-4 welcome-msg fade">
-              <h4>Welcome!</h4>
-              <p className="welcome-blurb">
+        {/* <div className="welcome"> */}
+        {/* <div className="row lineup-row"> */}
+        <div className='lineup-row'>
+          <div className="puppy-lineup fade">
+            <img className="together" src={MomAndPups} alt="mom and puppies together" />
+          </div>
+          <div className="welcome-msg fade">
+            <h4>Welcome!</h4>
+            <p className="welcome-blurb">
               After being involved with the PON breed for over eighteen years,
                 I have decided to start a breeding program here in Northeast Ohio.
                 My foundation bitch is Layla (3 years old), 
@@ -39,75 +42,75 @@ class Main extends Component {
                 I want them to ultimately be as much a member of the 
                 family as well as great champions. Please enjoy the
                 website, and watch for updates as we grow!</p>
-            </div>
-          </div>
-
-
-          <div className="container container-main">
-            <div className='row'>
-              <div className='col-md-12 video-player'>
-                <ReactPlayer 
-                  className='react-player'
-                  url={vidUrl}
-                  // light
-                  controls
-                  width='100%'
-                  height='100%'
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12 updates-blog">
-                <img src={Pumpkin3} className="puppy-pumpkin" alt="puppy with pumpkins" />
-                <img src={Pumpkin1} className="puppy-pumpkin" alt="puppy with pumpkins" />
-                <img src={Pumpkin2} className="puppy-pumpkin" alt="puppy with pumpkins" />
-                <img src={Pumpkin4} className="puppy-pumpkin" alt="puppy with pumpkins" />
-                <img src={Pumpkin} className="puppy-pumpkin" alt="puppy with pumpkins" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="announcement">
-                <div className="col-md-3">
-                </div>
-                <div className="col-md-6 announce-msg">
-                  <h4>Announcing...</h4>
-                  <p>{`We are pleased to announce the birth of Simon and Layla's puppies born August 8, 2018!
-                    Descending from a long line of AKC champions, the puppies' parents have been tested for PRA,
-                    hip dysplasia, CERF and are CHIC certified.`}</p>
-                </div>
-                <div className="col-md-3">
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-1">
-              </div>
-              <div className="col-med-4">
-                <div className="card">
-                  <img className="card-img-top announce-pic" src={Simon} alt="simon" />
-                  <div className="card-body">
-                    <h6 className="card-text">Sire</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="col-med-2">
-                <div className="empty" />
-              </div>
-              <div className="col-med-4">
-                <div className="card">
-                  <img className="card-img-top announce-pic" src={Layla} alt="layla" />
-                  <div className="card-body">
-                    <h6 className="card-text">Dam</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-1">
-              </div>
-            </div>
-          </div>
-          <div className="col-md-1">
           </div>
         </div>
+
+
+        <div className="container container-main">
+          <div className='row'>
+            <div className='col-md-12 video-player'>
+              <ReactPlayer 
+                className='react-player'
+                url={vidUrl}
+                // light
+                controls
+                width='100%'
+                height='100%'
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 updates-blog">
+              <img src={Pumpkin3} className="puppy-pumpkin" alt="puppy with pumpkins" />
+              <img src={Pumpkin1} className="puppy-pumpkin" alt="puppy with pumpkins" />
+              <img src={Pumpkin2} className="puppy-pumpkin" alt="puppy with pumpkins" />
+              <img src={Pumpkin4} className="puppy-pumpkin" alt="puppy with pumpkins" />
+              <img src={Pumpkin} className="puppy-pumpkin" alt="puppy with pumpkins" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="announcement">
+              <div className="col-md-3">
+              </div>
+              <div className="col-md-6 announce-msg">
+                <h4>Announcing...</h4>
+                <p>{`We are pleased to announce the birth of Simon and Layla's puppies born August 8, 2018!
+                    Descending from a long line of AKC champions, the puppies' parents have been tested for PRA,
+                    hip dysplasia, CERF and are CHIC certified.`}</p>
+              </div>
+              <div className="col-md-3">
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-1">
+            </div>
+            <div className="col-med-4">
+              <div className="card">
+                <img className="card-img-top announce-pic" src={Simon} alt="simon" />
+                <div className="card-body">
+                  <h6 className="card-text">Sire</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-med-2">
+              <div className="empty" />
+            </div>
+            <div className="col-med-4">
+              <div className="card">
+                <img className="card-img-top announce-pic" src={Layla} alt="layla" />
+                <div className="card-body">
+                  <h6 className="card-text">Dam</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-1">
+            </div>
+          </div>
+        </div>
+        <div className="col-md-1">
+        </div>
+        {/* </div> */}
         <div className="row">
           <div className="col-md-12">
             <div className="empty" />
