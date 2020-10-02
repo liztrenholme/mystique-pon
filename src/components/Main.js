6986bcf7545a0d4ff2b191e544e4c50a30b39699
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-10-01 21:51:29 -0400

    added new font, rearrnged divs, removed bootstrap in most places

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ import Pumpkin4 from '../images/pumpkin4.JPEG';
 import Simon from '../images/simon.JPG';
 import Layla from '../images/headshot.JPG';
 import MomAndPups from '../images/together-nature.png';
-import PuppyBackground from '../images/puppy1.JPG';
+// import PuppyBackground from '../images/puppy1.JPG';
 import ReactPlayer from 'react-player';
 
 class Main extends Component {
@@ -25,13 +25,15 @@ class Main extends Component {
 
         {/* <div className="welcome"> */}
         {/* <div className="row lineup-row"> */}
-        <div className='lineup-row'>
-          <div className="puppy-lineup fade">
-            <img className="together" src={MomAndPups} alt="mom and puppies together" />
-          </div>
-          <div className="welcome-msg fade">
-            <h4>Welcome!</h4>
-            <p className="welcome-blurb">
+        <div className='content-container'>
+          <div>
+            <div className='lineup-row'>
+              <div className="puppy-lineup fade">
+                <img className="together" src={MomAndPups} alt="mom and puppies together" />
+              </div>
+              <div className="welcome-msg fade">
+                <h4>Welcome!</h4>
+                <p className="welcome-blurb">
               After being involved with the PON breed for over eighteen years,
                 I have decided to start a breeding program here in Northeast Ohio.
                 My foundation bitch is Layla (3 years old), 
@@ -42,12 +44,12 @@ class Main extends Component {
                 I want them to ultimately be as much a member of the 
                 family as well as great champions. Please enjoy the
                 website, and watch for updates as we grow!</p>
+              </div>
+            </div>
           </div>
-        </div>
 
 
-        <div className="container container-main">
-          <div className='row'>
+          <div className="container container-main">
             <div className='col-md-12 video-player'>
               <ReactPlayer 
                 className='react-player'
@@ -58,64 +60,57 @@ class Main extends Component {
                 height='100%'
               />
             </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12 updates-blog">
-              <img src={Pumpkin3} className="puppy-pumpkin" alt="puppy with pumpkins" />
-              <img src={Pumpkin1} className="puppy-pumpkin" alt="puppy with pumpkins" />
-              <img src={Pumpkin2} className="puppy-pumpkin" alt="puppy with pumpkins" />
-              <img src={Pumpkin4} className="puppy-pumpkin" alt="puppy with pumpkins" />
-              <img src={Pumpkin} className="puppy-pumpkin" alt="puppy with pumpkins" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="announcement">
-              <div className="col-md-3">
+            <div>
+              <div className="updates-blog">
+                <img src={Pumpkin3} className="puppy-pumpkin" alt="puppy with pumpkins" />
+                <img src={Pumpkin1} className="puppy-pumpkin" alt="puppy with pumpkins" />
+                <img src={Pumpkin2} className="puppy-pumpkin" alt="puppy with pumpkins" />
+                <img src={Pumpkin4} className="puppy-pumpkin" alt="puppy with pumpkins" />
+                <img src={Pumpkin} className="puppy-pumpkin" alt="puppy with pumpkins" />
               </div>
-              <div className="col-md-6 announce-msg">
-                <h4>Announcing...</h4>
-                <p>{`We are pleased to announce the birth of Simon and Layla's puppies born August 8, 2018!
+            </div>
+            <div>
+              <div className="announcement">
+                <div className="announce-msg">
+                  <h4>Announcing...</h4>
+                  <p>{`We are pleased to announce the birth of Simon and Layla's puppies born August 8, 2018!
                     Descending from a long line of AKC champions, the puppies' parents have been tested for PRA,
                     hip dysplasia, CERF and are CHIC certified.`}</p>
-              </div>
-              <div className="col-md-3">
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-1">
-            </div>
-            <div className="col-med-4">
-              <div className="card">
-                <img className="card-img-top announce-pic" src={Simon} alt="simon" />
-                <div className="card-body">
-                  <h6 className="card-text">Sire</h6>
+                </div>
+                <div>
                 </div>
               </div>
             </div>
-            <div className="col-med-2">
-              <div className="empty" />
-            </div>
-            <div className="col-med-4">
-              <div className="card">
-                <img className="card-img-top announce-pic" src={Layla} alt="layla" />
-                <div className="card-body">
-                  <h6 className="card-text">Dam</h6>
+            <div className="row">
+              <div>
+                <div className="card">
+                  <img className="card-img-top announce-pic" src={Simon} alt="simon" />
+                  <div className="card-body">
+                    <h6 className="card-text">Sire</h6>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-1">
+              <div className="col-med-4">
+                <div className="card">
+                  <img className="card-img-top announce-pic" src={Layla} alt="layla" />
+                  <div className="card-body">
+                    <h6 className="card-text">Dam</h6>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-1">
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-md-1">
-        </div>
+        {/* <div className="col-md-1">
+        </div> */}
         {/* </div> */}
-        <div className="row">
+        {/* <div className="row">
           <div className="col-md-12">
             <div className="empty" />
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
